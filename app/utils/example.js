bc6eef42d6556d526bb2e22d520691a2c58ce5b9
proshunsuke <shunsuke0901@gmail.com>
2016-11-09 02:13:16 +0900

    例追加

--- app/utils/example.js
+++ app/utils/example.js
@@ -1,11 +1,31 @@
 // @flow
-import nicoClient from './niconico/nico-client'
+import nicoClient from './niconico/nicoClient'
 
 const email = process.argv[2];
 const password = process.argv[3];
 
 let client = new nicoClient();
+// ログイン
 client.login(email, password)
   .then(function(session){
     console.log(session);
   });
+
+const live_number = 281432864;
+const session = 'user_session=user_session_13294831_cc4532331cdd38e320f524400869ddfc684c590a7921167b9fa7496bf071a602;';
+
+// コメント取得
+client.liveComments(live_number, session, (comment) => {
+  console.log(
+    `コメ番: ${comment['no']}\tユーザーID: ${comment['user_id']}\t時間: ${timestampToDateformat(comment['date'])}\t
+    コメント: ${comment['comment']}\tプレミア: ${comment['premium']}\t NGスコア: ${comment['score']}`
+  );
+});
+
+function timestampToDateformat(timestamp){
+  const date = new Date(timestamp * 1000);
+  const hour    = date.getHours();
+  const minute  = date.getMinutes();
+  const seconds = date.getSeconds();
+  return `${hour}:${minute}:${seconds}`;
+}
