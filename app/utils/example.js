a9ad8a8ed24ce05a53cb523bc19b3ee144bcb785
proshunsuke <shunsuke0901@gmail.com>
2016-11-09 02:17:09 +0900

    実行例

--- app/utils/example.js
+++ app/utils/example.js
@@ -1,4 +1,11 @@
 // @flow
+// flow; babel-node app/utils/example.js email password
+//
+//コメ番: 430     ユーザーID: 31682722    時間: 2:14:53    コメント: ゲーミングＰＣ買えば解決だ、さっさと買え  プレミア: 1      NGスコア: undefined
+//コメ番: 431     ユーザーID: 29063508    時間: 2:14:54    コメント: 人生いろいろパソコンいろいろ      プレミア: undefined      NGスコア: undefined
+//コメ番: 432     ユーザーID: nMcbSh1JZNh-WsIDNnlb5VxlVqA 時間: 2:14:55    コメント: 文字が多い３行で  プレミア: 1      NGスコア: -1632
+//コメ番: 433     ユーザーID: 11933424    時間: 2:15:5    コメント: 毒飲め    プレミア: 1      NGスコア: undefined
+
 import nicoClient from './niconico/nicoClient'
 
 const email = process.argv[2];
